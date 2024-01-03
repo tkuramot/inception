8ae4063c322055577b853ae8beb96dcac3f4c2dd
tkuramot <mabdman512@gmail.com>
2024-01-04 02:31:15 +0900

    wp-cli conf

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 set -eux
 
-directory="/var/www/html/wordpress"
+directory="/var/www/html"
+config_file="${directory}/wp-config.php"
 
 # Set up WordPress website using wp-cli if it's not configured yet.
-if [ ! -f "${directory}/wp-config.php" ]; then
-  wp config create \
-    --dbhost="${WORDPRESS_DB_HOST}" \
-    --dbuser="${WORDPRESS_DB_USER}" \
-    --dbpass="${WORDPRESS_DB_PASSWORD}" \
-    --dbname="${WORDPRESS_DB_NAME}"
-    --path="${directory}"
-
-  wp core install \
-    --url="${DOMAIN_NAME}" \
-    --title="${WORDPRESS_TITLE}" \
-    --admin_user="${WORDPRESS_ADMIN_USER}" \
-    --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
-    --admin_email="${WORDPRESS_ADMIN_EMAIL}"
-    --path="${directory}"
-
+if [ ! -f "$config_file" ]; then
+  wp core download
+  wp config create
+  wp db create
+  wp core install
   wp option update permalink_structure /%postname%/
 fi
 
 tail -f /dev/null
+
