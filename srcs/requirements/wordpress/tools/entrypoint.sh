4aff3d5ef7071eec19f0f8c8093a764d4e73f31f
tkuramot <mabdman512@gmail.com>
2024-01-04 17:58:02 +0900

    fix env

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -5,20 +5,32 @@ directory="/var/www/html"
 index_file="${directory}/index.php"
 config_file="${directory}/wp-config.php"
 
+env
+
 # check if wordpress is already downloaded
 if [ ! -f "$index_file" ]; then
-  gosu www-data wp core download
+  gosu www-data wp core download --locale=ja --version="${WORDPRESS_VERSION}"
 fi
 
 # check if wp-config.php exists
 if [ ! -f "$config_file" ]; then
-  gosu www-data wp config create --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${WORDPRESS_DB_HOST}
+  gosu www-data wp config create \
+    --dbname="${WORDPRESS_DB_NAME}" \
+    --dbuser="${WORDPRESS_DB_USER}" \
+    --dbpass="${WORDPRESS_DB_PASSWORD}" \
+    --dbhost="${WORDPRESS_DB_HOST}"
   gosu www-data wp db create
 fi
 
 # check if WordPress is already installed
 if ! gosu www-data wp core is-installed; then
-  gosu www-data wp core install
+  gosu www-data wp core install \
+    --url="${DOMAIN_NAME}" \
+    --title="${WORDPRESS_TITLE}" \
+    --admin_user="${WORDPRESS_ADMIN_USER}" \
+    --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
+    --admin_email="${WORDPRESS_ADMIN_EMAIL}" \
+    --skip-email
   gosu www-data wp option update permalink_structure /%postname%/
 fi
 
